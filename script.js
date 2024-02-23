8e1672c39dc5a1ccaa898861d5a26a5bc3f67b4e
luana_zamperlini <luazamperlini@gmail.com>
2024-02-22 20:26:53 -0600

    Code corrections to another submition

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//********************************** API URL *********************
 const API_URL =
-  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FTB-MT-WEB-PT/events";
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FTM-MT-WEB-PT/events";
 
-//********************SELECTORS*********************
+//********************************* SELECTORS*********************
 
 //state object is going to store everything
 //A state is a JavaScript object that stores the dynamic data of a component and allows it to follow changes between renderings
@@ -9,90 +10,111 @@ const state = {
   events: [],
 };
 
+//******************** SELECTORS *********************
+
 const eventsList = document.querySelector("#events");
 const addEventForm = document.querySelector("#addEvent");
-//async stops the program to run to take its time to fetch all the data it needs. Whenever it is assigned, it returns some kind of promise
 
-//Get Request - Read
+//******************** METHODS *********************
+//Async stops the program to run to take its time to fetch all the data it needs.
+//Whenever it is assigned, it returns some kind of promise
+
+//CRUD METHODS:
+
+//**************** GET Request or - Read part of the CRUD ************
+//needs the async keyword, the function and the name of the function.
+// async tells the server to wait while its collecting all th data it needs to respond.
 async function getEvents() {
+  //try/catch block:
   try {
-    //fetch/get the initial data
+    //fetch gets the initial data since we are only passing the URL
+    //on inspect, we expect a ""true response", add the "await to have the time to collect all the data
     const response = await fetch(API_URL);
-    console.log("response", response);
+    //we only get a documentation in "json" that needs to be converted so we can get the actual data
     const json = await response.json();
+    //we add "data" to specify what you want to return from the database
     console.log("json", json.data);
     state.events = json.data;
   } catch (error) {
-    console.error(error, "There was an error /GET EVENTS");
+    console.error(error, "There was an error /GET events");
   }
 }
 getEvents();
 
-//CRUD METHODS
-//CREATE REQUEST - POST
-
+//******************** CREATE Request = POST **********************
+// pass the objects you need on the page:name, date,time...)
 async function createEvent(name, date, time, location, description) {
   try {
+    //At this time we are sending information back. The "fetch" will need an argument because by default it is a "GET Request" and we are CREATING a Request. We need to add method and header.
     const response = await fetch(API_URL, {
       method: "POST",
-      header: {
-        "Content-Type": "application/json",
-      },
+      //inform the way you want the application to run. json.
+      headers: { "Content-Type": "application/json" },
+      //"JSON.stringify" converts the information into json.
       body: JSON.stringify({ name, date, time, location, description }),
     });
 
-    const json = response.json();
+    const json = await response.json();
+    if (json.error) {
+      throw new error(json.message);
+    }
+  } catch (error) {
+    console.error(error, "There was an error /POST events");
+  }
+}
 
-    //Error handling
+createEvent(
+  "Event Name",
+  "2021-09-30",
+  "12:00:00",
+  "123 Street",
+  "This is a description of the event."
+);
+
+//******************** UPDATE Request = PUT **********************
+
+async function updateEvent(id, name, date, time, location, description) {
+  try {
+    //At this time we are sending information back. The "fetch" will need an argument because by default it is a "GET Request" and we are CREATING a Request. We need to add method and header.
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      //inform the way you want the application to run. json.
+      headers: { "Content-Type": "application/json" },
+      //"JSON.stringify" converts the information into json.
+      body: JSON.stringify({ name, date, time, location, description }),
+    });
+
+    const json = await response.json();
+    console.log("jason", json);
     if (json.error) {
       throw new Error(json.message);
     }
   } catch (error) {
-    console.error(error, "There was an error /POST EVENTS");
+    console.error(error, "There was an error /PUT events");
   }
-
-
-  createEvent(Birthday));
 }
 
-// id: 1,
-// eventName: "Event Name"
-// date: "2021-09-30T00:00:00.000Z", // Date ISO string
-// time: "12:00:00", // Time string
-// location: "123 Street"
-// description: "This is a description of the event.",
+updateEvent(
+  2111,
+  "Birthday Party",
+  "10/20/24",
+  "18:00",
+  "Kid City Theater",
+  "Special Surprise"
+);
 
-//delete button
-//form with a submit button
-//it should be added to the events list
+//******************** DELETE Request = DELETE**********************
+async function deleteEvent(id) {
+  try {
+    const response = await fetch(`$API_URL/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error("Event could not be deleted!");
+    }
+  } catch (error) {
+    console.error(error, "There was an error /DELETE event");
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// getGuest();
-// {
-//   id: 1,
-//   guestName: "Guest Name",
-//   email: "guest@email.com",
-//   phone: "123-456-7890"
-// }
-
-// getRSVP();
-// {
-//   id: 1,
-//   guestId: 1, // Id of the attending guest
-//   eventId: 1  // Id of the event being attended
-// }
+deleteEvent(2431);
